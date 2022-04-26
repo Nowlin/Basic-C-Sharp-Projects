9120e67c6e7dde359d877a635d9f0af73afcb019
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-04-26 12:23:59 -0500

    Assignment Submission Attempt 2

--- MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/Program.cs
@@ -13,7 +13,7 @@ namespace MathAndComparisonOperators
 
             Console.WriteLine("Please input a number to have Twenty-Five added to");
             int addition = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your number plus Twenty-Five is " + 25 + addition);
+            Console.WriteLine("Your number plus Twenty-Five is " + (25 + addition));
             Console.ReadLine();
 
             Console.WriteLine("Please input a number that will be divided by Twelve-point-five");
@@ -22,11 +22,11 @@ namespace MathAndComparisonOperators
             Console.ReadLine();
 
             Console.WriteLine("Please input a number and see whether it is greater than Fifty");
-            bool greater = 50 >= (Console.ReadLine());
-            Console.WriteLine("Is your number greater than Fifty? ", "It is " + greater);
+            int greater = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Is your number greater than Fifty? " + (greater > 50));
             Console.ReadLine();
 
-            Console.WriteLine("Please input a number that will be divided by Sven and you will be shown the remainder.");
+            Console.WriteLine("Please input a number that will be divided by Seven and you will be shown the remainder.");
             int remainder = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("The remainder of your number divided by Seven is " + remainder % 7);
             Console.ReadLine();
