92acbaab85c6fdffd69be2d2df5bdbda86419c78
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-04-29 16:00:57 -0500

    Loop Assignment Submission 1

---
+++ Loop Assignment Submission/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Loop_Assignment_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool answer = false;
+
+            while (answer == false)
+            {
+                Console.WriteLine("What is your favorite color?");
+                string favColor = Console.ReadLine();
+
+                favColor = favColor.ToLower();
+                if (favColor == "green")
+                {
+                    Console.WriteLine("That's right, green is the best color!");
+                    answer = true;
+                }
+                else
+                {
+                    Console.WriteLine("incorrect, " + favColor + " is a terrible color.");
+                }
+            }
+            Console.WriteLine("Please press enter to move onto the next time I will try and tell you how wrong you are!");
+            Console.ReadLine();
+
+
+            bool foodEntered = false;
+
+            do
+            {
+                Console.WriteLine("What is your favorite food?");
+                string favFood = Console.ReadLine();
+                favFood = favFood.ToLower();
+                switch (favFood)
+                {
+                    case "pizza":
+                        Console.WriteLine("Why would you choose pizza? Your choice is a \"dough\" able. Try again?");
+                        break;
+                    case "stir fry":
+                        Console.WriteLine("Are you trying to stir me up? Try again?");
+                        break;
+                    case "pasta":
+                        Console.WriteLine("Aren't we Pasta that? Try again?");
+                        break;
+                    case "burgers":
+                        Console.WriteLine("Whould you like fries with that? Try again?");
+                        break;
+                    default:
+                        Console.WriteLine("Are you kidding me? Try again?");
+                        break;
+                    case "bbq":
+                        Console.WriteLine("Save me some! Awesome choice!");
+                        foodEntered = true;
+                        break;
+                }
+                Console.ReadLine();
+            }
+            while (foodEntered == false);
+        }
+    }
+}
--- Price-Quote Application Assignment Submission/Program.cs
+++ Price-Quote Application Assignment Submission/Program.cs
@@ -13,7 +13,7 @@ namespace Price_Quote_Application_Assignment_Submission
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            if (packWeight <= 50)
+            else if (packWeight <= 50)
             {
                 Console.WriteLine("What is the width of the package?");
                 int packWidth = Convert.ToInt32(Console.ReadLine());
@@ -21,16 +21,16 @@ namespace Price_Quote_Application_Assignment_Submission
                 int packHeight = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("What is the length of the package?");
                 int packLength = Convert.ToInt32(Console.ReadLine());
-            if (packWidth + packHeight + packLength > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            }
-            if (packWidth + packHeight + packLength <= 50)
-            {
-                decimal totalCost = ((packHeight * packLength * packWidth) * packWeight) / 100;
-                Console.WriteLine("The total for shipping this package is: $" + totalCost);
-                Console.ReadLine();
-            }
+                if ((packWidth + packHeight + packLength) > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else if ((packWidth + packHeight + packLength) <= 50)
+                {
+                    decimal totalCost = ((packHeight * packLength * packWidth) * packWeight) / 100;
+                    Console.WriteLine("The total for shipping this package is: $" + totalCost);
+                    Console.ReadLine();
+                }
             }
         }
     }
