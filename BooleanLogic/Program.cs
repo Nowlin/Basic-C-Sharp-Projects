d8f80fe54b04fcf01234fac0a16c9e0a4590580c
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-04-29 13:14:06 -0500

    Price Quote Submission 1

--- BooleanLogic/Program.cs
+++ BooleanLogic/Program.cs
@@ -7,13 +7,13 @@ namespace BooleanLogic
         static void Main()
         {
             Console.WriteLine("To determine whether or not to approve a client's car insurance request, please fill in the following data.");
-            Console.WriteLine("What is the client's age?");
+            Console.WriteLine("What is the client's age? ex. 23");
             int clientAge = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Have they ever had a DUI, True or False?");
             bool clientDUI = Convert.ToBoolean(Console.ReadLine());
 
-            Console.WriteLine("How many speeding tickets have they been given?");
+            Console.WriteLine("How many speeding tickets have they been given? ex. 3");
             int clientTickets = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Is the client qualified?");
---
+++ Price-Quote Application Assignment Submission/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Price_Quote_Application_Assignment_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the package weight in pounds?");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            if (packWeight <= 50)
+            {
+                Console.WriteLine("What is the width of the package?");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of the package?");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length of the package?");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+            if (packWidth + packHeight + packLength > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            if (packWidth + packHeight + packLength <= 50)
+            {
+                decimal totalCost = ((packHeight * packLength * packWidth) * packWeight) / 100;
+                Console.WriteLine("The total for shipping this package is: $" + totalCost);
+                Console.ReadLine();
+            }
+            }
+        }
+    }
+}
