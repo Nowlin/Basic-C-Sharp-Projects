086a351bb72f2020ddd6d6d05d005dd27f36e715
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-04-29 11:47:07 -0500

    Boolean Logic Submission 1

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("To determine whether or not to approve a client's car insurance request, please fill in the following data.");
+            Console.WriteLine("What is the client's age?");
+            int clientAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have they ever had a DUI, True or False?");
+            bool clientDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets have they been given?");
+            int clientTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Is the client qualified?");
+            Console.WriteLine(clientAge > 15 && clientDUI == false && clientTickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
