bf6449f8b6a2f7485f01393baaffaf0347ee6049
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-07-06 13:18:51 -0500

    Classes and Methods Assignment 1 Submission

---
+++ Classes and Methods Assignment 1/CMAClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Methods_Assignment_1
+{
+    public class CMAClass
+    {
+        public int Add(int numOne)
+        {
+            int Sum = (numOne + 5);
+            return Sum
+        }
+    }
+}
---
+++ Classes and Methods Assignment 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMAClass Add = new CMAClass();
+            Console.WriteLine(Add.Add(3));
+            Console.WriteLine(Add.Deci(4.35m));
+            Console.WriteLine(Add.String("5"));
+        }
+    }
+}
