970b08cae0b134375f3bab466e3562d0dae0caf7
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-07-07 13:51:42 -0500

    Class Method Assignment Completion

---
+++ Class Method Assignment/CMA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Method_Assignment
+{
+    public class CMA
+    {
+        public void Divide(int divide)
+        {
+            int answer = (divide / 2);
+            Console.WriteLine(answer);
+        }
+        public int Divide(int times, int add, int subtract)
+        {
+            int Yo = (times * 5);
+            int Dis = (add + 3);
+            int Ard = (subtract - 11);
+            return Yo + Dis + Ard;
+        }
+        public static string NahBru(out string nahBru, out string nahBruh)
+        {
+            nahBru = "hello";
+            nahBruh = " world";
+            return nahBru + nahBruh;
+        }
+    }
+    public static class CMAdude
+    {
+
+    }
+}
