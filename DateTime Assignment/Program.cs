463d669ced4df6f7969b2d7138aa33f5a5167f46
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-08-04 10:00:49 -0500

    DateTime Assignment

---
+++ DateTime Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is: " + DateTime.Now + " at the time you started this page up. Please enter a number");
+            DateTime Hours = Convert.ToDateTime(Console.ReadLine());
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("In {1} hours it will be: ", Hours + (Hours + dateTime));
+        }
+    }
+}
