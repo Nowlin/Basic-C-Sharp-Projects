96d01a1a02f69bc6b8c9c99f2aaeeaec071a6f15
Nowlin <92047364+Nowlin@users.noreply.github.com>
2022-08-04 09:31:33 -0500

    File I/O Assignment

---
+++ File IO Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It would be great if you would enter a number....");
+            int log = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\s_now\logs\log.txt", true))
+            {
+                file.WriteLine(log);
+            }
+            File.ReadAllText(@"C:\Users\s_now\logs\log.txt");
+        }
+    }
+}
